43ea02204e37afae23152050ed5a8082f2074308
Procyonax <deoxtw@gmail.com>
2023-06-22 15:21:09 +0100

    added base spring framework and react app

---
+++ petpalservice/src/main/java/com/petpal/petpalservice/PetpalApplication.java
@@ -0,0 +1,13 @@
+package com.petpal.petpalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetpalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetpalApplication.class, args);
+	}
+
+}
---
+++ petpalservice/src/test/java/com/petpal/petpalservice/PetpalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petpal.petpalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetpalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
