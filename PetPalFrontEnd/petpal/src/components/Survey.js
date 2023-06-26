b618a652d9ca5be9dbe2e3cb456f38616b47eb29
Beth Lupton <luptonbeth21@gmail.com>
2023-06-26 13:22:06 +0100

    Comitting

--- PetPalFrontEnd/petpal/src/components/Survey.js
+++ PetPalFrontEnd/petpal/src/components/Survey.js
@@ -1,6 +1,183 @@
-const Survey = () => {
+const Survey = ({handleChange, handleSubmit, breedData}) => {
 
-    return ( "I am a Survey Component" );
+    return ( 
+        <>
+        <div>
+              <h1>Breed Form</h1>
+              <form onSubmit={handleSubmit}>
+                  <label>
+                      Breed Name:
+                      <input
+                          type="text"
+                          name="name"
+                          value={breedData.name}
+                          onChange={handleChange} />
+                  </label>
+                  <label>
+          Trainability:
+          <input
+            type="number"
+            name="trainability"
+            value={breedData.trainability}
+            onChange={handleChange}
+            min={0}
+            max={5}
+          />
+        </label>
+                  <br />
+                  <label>
+          Health Issues:
+          <input
+            type="number"
+            name="health_issues"
+            value={breedData.health_issues}
+            onChange={handleChange}
+            min={0}
+            max={5}
+          />
+        </label>
+        <br />
+        <label>
+          Exercise Needed:
+          <input
+            type="number"
+            name="exercise_needed"
+            value={breedData.exercise_needed}
+            onChange={handleChange}
+            min={0}
+            max={5}
+          />
+        </label>
+        <br />
+        <label>
+          Size:
+          <select
+            name="size"
+            value={breedData.size}
+            onChange={handleChange}
+          >
+            <option value="">Select size</option>
+            <option value="Small">Small</option>
+            <option value="Medium">Medium</option>
+            <option value="Large">Large</option>
+            <option value="Giant">Giant</option>
+          </select>
+        </label>
+        <br />
+        <label>
+          Drooling:
+          <input
+            type="number"
+            name="drooling"
+            value={breedData.drooling}
+            onChange={handleChange}
+            min={0}
+            max={5}
+          />
+        </label>
+        <br />
+        <label>
+          Grooming:
+          <input
+            type="number"
+            name="grooming"
+            value={breedData.grooming}
+            onChange={handleChange}
+            min={0}
+            max={5}
+          />
+        </label>
+        <br />
+        <label>
+          Hypoallergenic:
+          <input
+            type="number"
+            name="hypoallergenic"
+            value={breedData.hypoallergenic}
+            onChange={handleChange}
+            min={0}
+            max={5}
+          />
+        </label>
+        <br />
+        <label>
+          Barking:
+          <input
+            type="number"
+            name="barking"
+            value={breedData.barking}
+            onChange={handleChange}
+            min={0}
+            max={5}
+          />
+        </label>
+        <br />
+        <label>
+          Protective Rating:
+          <input
+            type="number"
+            name="protective_rating"
+            value={breedData.protective_rating}
+            onChange={handleChange}
+            min={0}
+            max={5}
+          />
+        </label>
+        <br />
+        <label>
+          Sociability:
+          <input
+            type="number"
+            name="sociability"
+            value={breedData.sociability}
+            onChange={handleChange}
+            min={0}
+            max={5}
+          />
+        </label>
+        <br />
+        <label>
+          Friendliness to Other Dogs:
+          <input
+            type="number"
+            name="friendliness_to_other_dogs"
+            value={breedData.friendliness_to_other_dogs}
+            onChange={handleChange}
+            min={0}
+            max={5}
+          />
+        </label>
+        <br />
+        <label>
+          Child Friendly:
+          <input
+            type="number"
+            name="child_friendly"
+            value={breedData.child_friendly}
+            onChange={handleChange}
+            min={0}
+            max={5}
+          />
+        </label>
+        <br />
+
+        <label>
+          Independence:
+          <input
+            type="number"
+            name="independence"
+            value={breedData.independence}
+            onChange={handleChange}
+            min={0}
+            max={5}
+          />
+        </label>
+        <br />
+                  <button type="submit">Submit</button>
+              </form>
+              </div>
+        </>
+     );
 }
  
 export default Survey;
--- PetPalFrontEnd/petpal/src/containers/PawPalContainer.js
+++ PetPalFrontEnd/petpal/src/containers/PawPalContainer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'; 
 import Survey from '../components/Survey';
 import Resources from '../components/Resources';
@@ -9,21 +9,59 @@ import Splash from '../components/Splash';
 
 const PawPalContainer = () => {
 
+  const [breedData, setBreedData] = useState({
+    name: "",
+    trainability: "",
+    health_issues: "",
+    exercise_needed: "",
+    size: "",
+    drooling: "",
+    grooming: "",
+    hypoallergenic: "",
+    barking: "",
+    protective_rating: "",
+    sociability: "",
+    friendliness_to_other_dogs: "",
+    child_friendly: "",
+    independence: ""
+  });
 
-    return ( 
-        <div>
-            <Router>
-                <NavBar/>
-                <Routes>
-                    <Route path="/" element={<Splash/>}/>
-                    <Route path="/survey" element={<Survey/>}/>
-                    <Route path="/resources" element={<Resources/>}/>
-                    <Route path="/atozlist" element={<AtoZList/>}/>
-                    <Route path="/detailbreed" element={<DetailBreed/>}/>
-                </Routes>
-            </Router>
-        </div>
-     );
-}
- 
-export default PawPalContainer;
+  useEffect(() => {
+    getBreedData()
+  }, []);
+
+  const getBreedData= () => {
+    return breedData;
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setBreedData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(breedData); // Perform desired actions with the form data
+  };
+
+  return (
+    <><div>
+          <Router>
+              <NavBar />
+              <Routes>
+                  <Route path="/" element={<Splash />} />
+                  <Route path="/survey" element={<Survey handleChange={handleChange} handleSubmit={handleSubmit} breedData={breedData}/>} />
+                  <Route path="/resources" element={<Resources />} />
+                  <Route path="/atozlist" element={<AtoZList />} />
+                  <Route path="/detailbreed" element={<DetailBreed />} />
+              </Routes>
+          </Router>
+      </div>
+    </>
+)}
+          
+          
+          export default PawPalContainer;
