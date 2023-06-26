ba73cf5e6b8a4a3b839afd2837cdf89345da0da9
Gregor <gregor.home@btinternet.com>
2023-06-26 09:08:24 +0100

    Added images folder to public front end, added mappings to controller

--- petService/src/main/java/com/example/petService/controller/BreedController.java
+++ petService/src/main/java/com/example/petService/controller/BreedController.java
@@ -1,13 +1,46 @@
 package com.example.petService.controller;
 
+import com.example.petService.models.Breed;
 import com.example.petService.repository.BreedRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class BreedController {
 
     @Autowired
     BreedRepository breedRepository;
+
+    @GetMapping(value="/breeds")
+    public ResponseEntity<List<Breed>> getAllBreeds(){
+        return new ResponseEntity<>(breedRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value="/breeds/{id}")
+    public ResponseEntity getBreed(@PathVariable Long id){
+        return new ResponseEntity<>(breedRepository.findById(id), HttpStatus.OK);
+    }
+    @PostMapping(value="/breeds")
+    public ResponseEntity<Breed> postBreed(@RequestBody Breed breed){
+        breedRepository.save(breed);
+        return new ResponseEntity<>(breed, HttpStatus.OK);
+    }
+    @DeleteMapping(value="/breeds/{id}")
+    public ResponseEntity<Breed> deleteBreed(@PathVariable Long id){
+        Breed found = breedRepository.getOne(id);
+        breedRepository.delete(found);
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+    @PatchMapping(value = "/breeds/{id}")
+    public ResponseEntity<Breed> updateBreed(@RequestBody Breed breed){
+        breedRepository.save(breed);
+        return new ResponseEntity<>(breed, HttpStatus.OK);
+    }
+
+
 
 }
