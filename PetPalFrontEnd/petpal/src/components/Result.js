073a5d76dc9e77a70abe81acc1a14263a22da38e
Scott Hynd <user@users-MacBook-Pro-10.local>
2023-06-29 16:24:20 +0100

    incomplete results page

--- PetPalFrontEnd/petpal/src/components/Result.js
+++ PetPalFrontEnd/petpal/src/components/Result.js
@@ -1,20 +1,33 @@
-import PawPalContainer from "../containers/PawPalContainer";
 import ResultCard from "../components/ResultCard";
 
 const Result = ({reversedArray}) => {
-
-  const breedItems = reversedArray.map((breed, index) => 
+    reversedArray.map((breed, index) => 
     <ResultCard  key={index} breed={breed}/>
   );
 
   return (
-    <div>
-      <ul>
-        <li>
-        <p>{breedItems}</p>
-        </li>
-      </ul>
+    <>
+    <div id="carouselExampleCaptions" className="carousel slide">
+    <div className="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
+    <div className="carousel-inner">
+      <div className="carousel-item active">
+        {/* <ResultCard breed={breed}/> */}
+      </div>
+    </div>
+    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Previous</span>
+    </button>
+    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span className="carousel-control-next-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Next</span>
+    </button>
+  </div>
+ </>
   );
 };
 
--- PetPalFrontEnd/petpal/src/components/ResultCard.js
+++ PetPalFrontEnd/petpal/src/components/ResultCard.js
@@ -1,11 +1,14 @@
-const ResultCard = ({key, breed}) => {
+const ResultCard = ({breed}) => {
 
-
+console.log("breed at resultcard" + breed)
   return (
     <div>
-      {/* <img src="../public/Images/"{bestMatch.image} alt="{}"/> */}
-      <h2>{breed.name}</h2>
-      <p>{breed.description}</p>
+      <img src={`../images/${breed.image}`} className="d-block w-100" alt="..."/>
+        <div className="carousel-caption d-none d-md-block">
+          <h5>{breed.name}</h5>
+          <p>{breed.description}</p>
+          <button className="btn">find at dog trust</button>
+        </div>
     </div>
   );
 };
