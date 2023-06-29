a62f837c953a4ba6a971126f72d4ca652c84a089
Beth Lupton <luptonbeth21@gmail.com>
2023-06-29 15:57:43 +0100

    adding more links and headings

--- PetPalFrontEnd/petpal/src/components/Resources.js
+++ PetPalFrontEnd/petpal/src/components/Resources.js
@@ -5,55 +5,87 @@ const Resources = () => {
   return (
     <>
       <h1>Pawsome Resources</h1>
-      <h2>
-        <a href="https://www.dogstrust.org.uk/" target="_blank">
+      <div>
+      <h2>Find your perfect pooch</h2>
+      <h3>
+        <a href="https://www.dogstrust.org.uk/" target="_blank" alt="{dogs_trust_logo}">
           <img
             src="../images/dogs_trust_logo.jpg"
             style={{ height: "100px" }}
           />
         </a>
-      </h2>
+      </h3>
       <p>
         When a dog is in distress, we care for them. When a dog needs a home, we
         find them a loving family. When an owner needs a helping hand (or paw)
         or they just can`t cope, we`re ready to step in.{" "}
       </p>
-      <h2>
-        <a href="https://www.battersea.org.uk/" target="_blank">
+      <h3>
+        <a href="https://www.battersea.org.uk/" target="_blank" alt="{battersea_dog_and_cat_home_logo}">
           <img src="../images/battersea_logo.jpg" style={{ height: "100px" }} />
         </a>
-      </h2>
+      </h3>
       <p>
         At Battersea, we take in every animal who needs us. We give each one
         lots of love and expert care and get to know their characters and quirks
         so we can find them a new home that`s just right for them.
       </p>
-      <h2>
+      <h3>
         <a href="https://www.scottishspca.org/" target="_blank">
-          <img src="../images/spca_logo.jpg" style={{ height: "50px" }} />
+          <img src="../images/spca_logo.jpg" style={{ height: "50px" }} alt="{space_logo}"/>
         </a>
-      </h2>
+      </h3>
       <p>Scotland`s only all animal rescue team</p>
-      <h2>
+      <h3>
         <a href="https://www.rspca.org.uk/home" target="_blank">
-          <img src="../images/rspca_logo.jpg" style={{ height: "50px" }} />
+          <img src="../images/rspca_logo.jpg" style={{ height: "50px" }} alt="{rspca_logo}"/>
         </a>
-      </h2>
+      </h3>
       <p>
         Every year thousands of animals suffer from neglect, cruelty and abuse.
         With your help, we can end their misery. We rely on voluntary
         fundraising and donations to carry out our vital work.
       </p>
-      <h2>
+      <h3>
         <a href="https://www.bluecross.org.uk/" target="_blank">
-          <img src="../images/blue_cross_logo.jpg" style={{ height: "50px" }} />
+          <img src="../images/blue_cross_logo.jpg" style={{ height: "50px" }} alt="{blue_cross_logo}" />
         </a>
-      </h2>
+      </h3>
       <p>
         We find happy homes for sick, injured and homeless pets. We keep pets
         healthy by promoting responsible pet ownership and providing veterinary
         care to owners who can`t afford private veterinary fees.
       </p>
+      </div>
+      <div>
+        <h2>Find a vet near you!</h2>
+        <h3>
+        <a href="https://findavet.rcvs.org.uk/home/?&&type=rfst&set=true#cookie-widget" target="_blank">
+          <img src="../images/rcvs_logo.jpg" style={{ height: "80px" }} alt="{royal_college_of_veterinary_surgeons_logo}" />
+        </a>
+        </h3>
+        <p>
+        Welcome to our Find a Vet service ~ the most comprehensive information available about veterinary practices in the UK, the services they offer and the 
+        facilities they provide. You can also use this service to search for veterinary surgeons and veterinary nurses, and to check their registration status.
+        </p>
+      </div>
+      <div>
+        <h2>Pet Sitters for when you are away</h2>
+        <h3>
+        <a href="https://www.rover.com/" target="_blank">
+          <img src="../images/rover_logo.jpg" style={{ height: "50px" }} alt="{rover_logo}" />
+        </a>
+      </h3>
+      <p>Book trusted sitters and dog walkers. Meet local sitters who will treat your pets like family</p>
+      <h3>
+        <a href="https://www.trustedhousesitters.com/" target="_blank">
+          <img src="../images/trusted_house_sitters_logo.jpg" style={{ height: "50px" }} alt="{trusted_house_sitters_logo}"  />
+        </a>
+      </h3>
+      <p>
+      Find a verified and reviewed sitter who`ll keep your pets company and give them all the time, care and attention in the world.
+      </p>
+      </div>
     </>
   );
 };
@@ -61,5 +93,5 @@ const Resources = () => {
 export default Resources;
 
 {
-  /* <p><a href="https://www.Youtube.com/"><img src="https://www.tutorialspoint.com/assets/questions/media/426142-1668760872.png" style="width:50px;height:50px;"></a></p> */
+ 
 }
