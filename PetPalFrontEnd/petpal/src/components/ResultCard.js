14130a91b2f3c1623ff085d0499fb746087e3fe4
Scott Hynd <user@users-MacBook-Pro-10.local>
2023-07-02 10:36:23 +0100

    working carousel and updated styling

--- PetPalFrontEnd/petpal/src/components/ResultCard.js
+++ PetPalFrontEnd/petpal/src/components/ResultCard.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import './Result.css'
+import '../App.css'
 
-const ResultCard = ({ key, breed }) => {
+const ResultCard = ({breed}) => {
   return (
     <div>
-      <img src={`../Images/${breed.image}`} alt="{}" />
-      <h2>{breed.name}</h2>
-      <p>{breed.description}</p>
+      <img src={`../Images/${breed.image}`} alt={`${breed.name}`} className="carousel-img" />
+      <div>
+        <h2 className='result-card-name'>{breed.name}</h2>
+      </div>
+      <p className='result-card-description'>{breed.description}</p>
     </div>
   );
 };
