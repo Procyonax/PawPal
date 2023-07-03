f33327105ba52b9a22b864e618251a66d43f5c10
Procyonax <deoxtw@gmail.com>
2023-07-03 10:31:54 +0100

    added survery component to css

--- PetPalFrontEnd/petpal/src/components/Survey.js
+++ PetPalFrontEnd/petpal/src/components/Survey.js
@@ -49,67 +49,67 @@ const Survey = ({handleChange, handleSubmit, formData}) => {
   const slides = [
     {
       name: "trainability",
-      label: "Trainability",
+      label: "On a scale of 1 to 5, how important is it to you that your new dog is easily trainable?",
       value: breedData.trainability,
     },
     {
       name: "health_issues",
-      label: "Health Issues",
+      label: "How concerned are you about potential health issues in your new dog, on a scale of 1 to 5?",
       value: breedData.health_issues,
     },
     {
       name: "exercise_needed",
-      label: "Exercise Needed",
+      label: "When it comes to exercise needs, how would you rate your preference for your new dog on a scale of 1 to 5?",
       value: breedData.exercise_needed,
     },
     {
       name: "size",
-      label: "Size",
+      label: "Considering the size of your new dog, where would you place your preference on a scale of 1 to 5?",
       value: breedData.size,
     },
     {
       name: "drooling",
-      label: "Drooling",
+      label: "How comfortable are you with a dog that drools, on a scale of 1 to 5?",
       value: breedData.drooling,
     },
     {
       name: "grooming",
-      label: "Grooming",
+      label: "On a scale of 1 to 5, how much grooming are you willing to undertake for your new dog?",
       value: breedData.grooming,
     },
     {
       name: "hypoallergenic",
-      label: "Hypoallergenic",
+      label: "Are you looking for a hypoallergenic dog? Please rate your preference on a scale of 1 to 5.",
       value: breedData.hypoallergenic,
     },
     {
       name: "barking",
-      label: "Barking",
+      label: "How tolerant are you of barking in your new dog, on a scale of 1 to 5?",
       value: breedData.barking,
     },
     {
       name: "protective_rating",
-      label: "Protective Rating",
+      label: "Considering your desire for a protective dog, how would you rate your preference on a scale of 1 to 5?",
       value: breedData.protective_rating,
     },
     {
       name: "sociability",
-      label: "Sociability",
+      label: "On a scale of 1 to 5, how important is it to you that your new dog is sociable?",
       value: breedData.sociability,
     },
     {
       name: "friendliness_to_other_dogs",
-      label: "Friendliness to Other Dogs",
+      label: "How friendly would you like your new dog to be towards other dogs, on a scale of 1 to 5?",
       value: breedData.friendliness_to_other_dogs,
     },
     {
       name: "child_friendly",
-      label: "Child Friendly",
+      label: "When it comes to your new dog's friendliness towards children, where would you place your preference on a scale of 1 to 5?",
       value: breedData.child_friendly,
     },
     {
       name: "independence",
-      label: "Independence",
+      label: "On a scale of 1 to 5, how independent would you like your new dog to be?",
       value: breedData.independence,
     },
   ];
@@ -118,7 +118,7 @@ const Survey = ({handleChange, handleSubmit, formData}) => {
   const isFinalSlide = currentSlide === slides.length - 1;
 
   return (
-    <div>
+    <div className='survey-container'>
       <h1></h1>
       <form onSubmit={handleSubmit}>
         <div>
