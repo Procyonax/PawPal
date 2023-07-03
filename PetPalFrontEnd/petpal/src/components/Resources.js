a0ae1590dcb3506ff27e47e0a7f0bd0eff2c45fd
Beth Lupton <luptonbeth21@gmail.com>
2023-07-03 23:13:37 +0100

    added some more styling to resources page

--- PetPalFrontEnd/petpal/src/components/Resources.js
+++ PetPalFrontEnd/petpal/src/components/Resources.js
@@ -17,11 +17,14 @@ const Resources = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img className="logos-larger" src="../images/dogs_trust_logo.jpg" alt="{dogs_trust_logo}"/>
-
+            <img
+              className="logos-larger"
+              src="../images/dogs_trust_logo.jpg"
+              alt="{dogs_trust_logo}"
+            />
           </a>
         </h3>
-        <p>
+        <p className="resources-information">
           When a dog is in distress, we care for them. When a dog needs a home,
           we find them a loving family. When an owner needs a helping hand (or
           paw) or they just can`t cope, we`re ready to step in.{" "}
@@ -41,13 +44,17 @@ const Resources = () => {
             />
           </a>
         </h3>
-        <p>
+        <p className="resources-information">
           At Battersea, we take in every animal who needs us. We give each one
           lots of love and expert care and get to know their characters and
           quirks so we can find them a new home that`s just right for them.
         </p>
         <h3>
-          <a href="https://www.scottishspca.org/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.scottishspca.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img
               className="logo-sspca"
               src="../images/spca_logo.jpg"
@@ -55,7 +62,7 @@ const Resources = () => {
             />
           </a>
         </h3>
-        <p>
+        <p className="resources-information">
           As Scotland's animal welfare charity, we've been on-hand to protect
           animals and prevent cruelty for 180 years. Over almost two centuries,
           we've grown to become a national charity which celebrates the strength
@@ -63,7 +70,11 @@ const Resources = () => {
           We are Scotland's animal champions.
         </p>
         <h3>
-          <a href="https://www.rspca.org.uk/home" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.rspca.org.uk/home"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img
               className="logos"
               src="../images/rspca_logo.jpg"
@@ -71,13 +82,17 @@ const Resources = () => {
             />
           </a>
         </h3>
-        <p>
+        <p className="resources-information">
           Every year thousands of animals suffer from neglect, cruelty and
           abuse. With your help, we can end their misery. We rely on voluntary
           fundraising and donations to carry out our vital work.
         </p>
         <h3>
-          <a href="https://www.bluecross.org.uk/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.bluecross.org.uk/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img
               className="logos"
               src="../images/blue_cross_logo.jpg"
@@ -85,7 +100,7 @@ const Resources = () => {
             />
           </a>
         </h3>
-        <p>
+        <p className="resources-information">
           We find happy homes for sick, injured and homeless pets. We keep pets
           healthy by promoting responsible pet ownership and providing
           veterinary care to owners who can`t afford private veterinary fees.
@@ -110,7 +125,7 @@ const Resources = () => {
             />
           </a>
         </h3>
-        <p>
+        <p className="resources-information">
           Welcome to our Find a Vet service ~ the most comprehensive information
           available about veterinary practices in the UK, the services they
           offer and the facilities they provide. You can also use this service
@@ -121,11 +136,15 @@ const Resources = () => {
       <div div className="resources">
         <h2>
           <img src={PawPrint} aria-hidden alt="paw-print-icon" />
-          Pet Sitters for when you are away
+          Pet sitters for when you are away
           <img src={PawPrint} aria-hidden alt="paw-print-icon" />
         </h2>
         <h3>
-          <a href="https://www.rover.com/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.rover.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img
               className="logos"
               src="../images/rover_logo.jpg"
@@ -133,12 +152,16 @@ const Resources = () => {
             />
           </a>
         </h3>
-        <p>
+        <p className="resources-information">
           Book trusted sitters and dog walkers. Meet local sitters who will
           treat your pets like family.
         </p>
         <h3>
-          <a href="https://www.trustedhousesitters.com/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.trustedhousesitters.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img
               className="logos"
               src="../images/trusted_house_sitters_logo.jpg"
@@ -146,7 +169,7 @@ const Resources = () => {
             />
           </a>
         </h3>
-        <p>
+        <p className="resources-information">
           Find a verified and reviewed sitter who`ll keep your pets company and
           give them all the time, care and attention in the world.
         </p>
